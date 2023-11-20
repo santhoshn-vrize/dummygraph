b4d79d7f57280afa6bfca3323a9d82ca93d2606d
SAN327905 <santhoshn641@gmail.com>
2023-11-21 00:27:14 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,45 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// import { Link } from 'next/link';
+// TODO explore Link component here is crashing the page
+"use client";
+import React from "react";
+
+import Chart from "../components/Graph";
+const graphbgurl =
+  "https://dev-p2p-synergy.s3.ap-south-1.amazonaws.com/chartbgimage+3.svg"; //default URL
 
 export default function Home() {
+  const labels = [
+    "New RFQs",
+    "Acknowledged RFQs",
+    "Ordered",
+    "Quoted RFQs",
+    "Acknowledged RFQs",
+    "Cancelled",
+  ]; // default label
+  const dataValues = [282, 132, 124, 233, 156, 160]; // default values
+  const activePrLabel = "# Invoice"; // default value
+  const title = "Summary of RFQs"; // default value
+  const sumLabel = 747; // default value
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      <Chart
+        labels={labels}
+        dataValues={dataValues}
+        activePrLabel={activePrLabel}
+        title={title}
+        sumLabel={sumLabel}
+        graphbgurl={graphbgurl}
+      />
+      {/* <CustomGraph
+        labels={labels}
+        dataValues={dataValues}
+        activePrLabel={activePrLabel}
+        title={title}
+        sumLabel={sumLabel}
+        graphbgurl={graphbgurl}
+      /> */}
+      {/* <Link href="/en/quote-requests">Quote Requests</Link> */}
+    </div>
+  );
 }
---
+++ src/components/Graph/index.tsx
@@ -0,0 +1,236 @@
+"use client";
+import React, { useEffect } from "react";
+import { Doughnut } from "react-chartjs-2";
+import {
+  DoughnutController,
+  registerables as _registerables,
+  ChartComponent,
+} from "chart.js/auto";
+import { Registry, ChartComponentLike } from "chart.js/auto";
+import * as Chart from "chart.js/auto";
+import "./styles.css";
+type DoughnutChartData = Chart.ChartData<"doughnut", number[], unknown>;
+
+interface ExtendedRegistry extends Registry {
+  register(...controllers: ChartComponentLike[]): void;
+  addControllers(...controllers: ChartComponentLike[]): void;
+}
+
+const registry = Chart.registry as ExtendedRegistry;
+
+interface CustomizableCardProps {
+  title: string;
+  chartData: DoughnutChartData;
+  activePrLabel: string;
+  sumLabel: number;
+  backgroundColors: string[];
+  graphbgurl: string;
+}
+
+const CustomizableCard: React.FC<CustomizableCardProps> = ({
+  title,
+  chartData,
+  activePrLabel,
+  sumLabel,
+  backgroundColors,
+  graphbgurl,
+}) => {
+  const dataValues: (number | null)[] = (
+    chartData.datasets?.[0]?.data || []
+  ).map((value: number | null) => (typeof value === "number" ? value : null));
+
+  const customOptions: {
+    cutout: string;
+    tooltips?: { enabled: boolean };
+  } & Chart.ChartOptions = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    cutout: "87%",
+    elements: {
+      arc: {
+        borderWidth: 0,
+      },
+    },
+    tooltips: {
+      enabled: false,
+    },
+    hover: {
+      mode: "nearest",
+    },
+  };
+
+  useEffect(() => {
+    if (registry && typeof registry.addControllers === "function") {
+      registry.addControllers(DoughnutController as ChartComponent);
+      return;
+    }
+
+    if (registry && "register" in registry) {
+      const extendedRegistry = registry as ExtendedRegistry;
+      (_registerables as ChartComponent[]).forEach((item) =>
+        extendedRegistry.register(item)
+      );
+    }
+  }, []);
+
+  return (
+    <div
+      className="customizable-card"
+      style={{
+        backgroundImage: `url(${graphbgurl})`,
+      }}
+    >
+      <div className="card-content">
+        <p>
+          <span className="card-title">{title}</span>
+        </p>
+        <div className="chart-container">
+          <Doughnut
+            data={chartData}
+            options={customOptions}
+            className="doughnut-chart"
+          />
+        </div>
+        <p className="active-label">{activePrLabel}</p>
+
+        <p className="sum-Label">{sumLabel}</p>
+      </div>
+      <div className="label-container">
+        {dataValues.map((value, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              color: "aliceblue",
+              alignItems: "center",
+
+              marginBottom: "2px",
+              fontFamily: "Poppins",
+            }}
+          >
+            <div
+              className="custom-badge-container"
+              style={{ backgroundColor: backgroundColors[index] }}
+            >
+              <div className="custom-badge-text">{value?.toString()}</div>
+            </div>
+            <div className="label-name">
+              {(chartData.labels?.[index] as React.ReactNode) || ""}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    //   </div>
+    // </div>
+  );
+};
+
+interface CardContainerProps {
+  children: React.ReactNode;
+}
+
+const CardContainer: React.FC<CardContainerProps> = ({ children }) => (
+  <div
+    style={{
+      display: "flex",
+
+      flexDirection: "row",
+      justifyContent: "space-between", // You can adjust this based on your layout preferences
+      width: "100%", // Adjust the width as needed
+    }}
+  >
+    {children}
+  </div>
+);
+
+interface AppProps {
+  labels?: string[];
+  dataValues?: number[];
+  activePrLabel: string;
+  title: string;
+  sumLabel: number;
+  graphbgurl: string;
+}
+
+const CustomGraph: React.FC<AppProps & { graphbgurl: string }> = ({
+  labels,
+  dataValues,
+  activePrLabel,
+  title,
+  sumLabel,
+  graphbgurl,
+}) => {
+  const backgroundColors = [
+    "rgba(149, 189, 255, 1)",
+    "rgba(248, 229, 164, 1)",
+    "rgba(247, 200, 224, 1)",
+    "rgba(140, 242, 242, 1)",
+    "rgba(255, 192, 179, 1)",
+    "rgba(223, 255, 216, 1)",
+  ];
+
+  useEffect(() => {
+    const linkElement = document.createElement("link");
+    linkElement.href = "https://fonts.googleapis.com/css?family=Poppins";
+    linkElement.rel = "stylesheet";
+    document.head.appendChild(linkElement);
+
+    return () => {
+      document.head.removeChild(linkElement);
+    };
+  }, []);
+
+  const data1: Chart.ChartData = {
+    labels: labels || [
+      "New RFQs",
+      "Ordered",
+      "Quoted RFQs",
+      "Order Lost",
+      "Declined RFQs",
+      "Cancelled",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: dataValues?.map((value) => (value !== null ? value : 0)) || [],
+        backgroundColor: backgroundColors,
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  return (
+    <CardContainer>
+      <CustomizableCard
+        title={title}
+        chartData={data1 as Chart.ChartData<"doughnut", number[], unknown>}
+        activePrLabel={activePrLabel}
+        sumLabel={sumLabel}
+        backgroundColors={backgroundColors}
+        graphbgurl={graphbgurl}
+      />
+      <CustomizableCard
+        title={title}
+        chartData={data1 as Chart.ChartData<"doughnut", number[], unknown>}
+        activePrLabel={activePrLabel}
+        sumLabel={sumLabel}
+        backgroundColors={backgroundColors}
+        graphbgurl={graphbgurl}
+      />
+      <CustomizableCard
+        title={title}
+        chartData={data1 as Chart.ChartData<"doughnut", number[], unknown>}
+        activePrLabel={activePrLabel}
+        sumLabel={sumLabel}
+        backgroundColors={backgroundColors}
+        graphbgurl={graphbgurl}
+      />
+    </CardContainer>
+  );
+};
+
+export default CustomGraph;
